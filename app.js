7046b8c156c07600db397a17fe46fe88e4996b17
danpaul <pt2323@gmail.com>
2013-08-16 13:32:25 -0400

    user input

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 var mongoose = require('mongoose'),
 	_ = require('underscore'),
  	async = require('async'),
- 	express = require('express');
+ 	express = require('express'),
+ 	engine = require('ejs-locals');
 
 var config = require('./config');
 var app = exports.app = express();
 
 mongoose.connect(config.dbURI);
 
+app.engine('ejs', engine);
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+
 app.locals.title = 'textNIL';
 
 app.get('*', function (req, res)
 {
   res.render('index');
+});
+
+app.post('/addnode', function (req, res)
+{
+//console.log(req.query);
+console.log(req.body.text);
+  res.send('in post');
 });
 
 app.listen(3000);
